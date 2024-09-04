5df76c8bd380c3226697a052b4a26d09e5642a00
Barron Fritz <barron.vonn.fritz@gmail.com>
2024-09-03 21:35:45 -0500

    Initial Commit. Template Project Build and Run Successful.

---
+++ src/main/java/net/takatalvi/takamc/TakatalviModClient.java
@@ -0,0 +1,10 @@
+package net.takatalvi.takamc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TakatalviModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/takatalvi/takamc/TakatalviModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.takatalvi.takamc;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TakatalviModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
